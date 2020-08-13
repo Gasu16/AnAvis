181d9131d61e37e2ea419258d35f42992052b7f0
Gasu16 <rosarioma.grammatico@studenti.unicam.it>
2020-08-13 23:09:08 +0200

    Removed initial interface Removed "about" page

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -2,7 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next){
-    res.send("CIAO MARIO! FACCIAMO COMUNICARE IL CLIENT (REACT) CON IL SERVER (NODE.JS)!"); // Mandiamo questo messaggio al server
+    res.render('testAPI', {title: 'testAPI'});
+    //res.send("CIAO MARIO! FACCIAMO COMUNICARE IL CLIENT (REACT) CON IL SERVER (NODE.JS)!"); // Mandiamo questo messaggio al server
 });
 
 module.exports = router;
