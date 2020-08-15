b891e3665b79bb892b02e41c037fc75dc5304272
Gasu16 <rosarioma.grammatico@studenti.unicam.it>
2020-08-16 01:50:13 +0200

    Migliorare gestore eventi button Registrati

--- client/src/components/registrarsi/registrarsi.js
+++ client/src/components/registrarsi/registrarsi.js
@@ -1,13 +1,123 @@
+/* 
+    PERCHÈ ALLA RIGA 148 QUANDO C'È <BUTTON ONCLICK={FUNZIONE JAVASCRIPT}>
+    SE METTO CONSOLE.LOG FUNZIONA
+    SE METTO UNA MIA FUNZIONE DEFINITA NELLA MIA CLASSE NON FUNZIONA?
+    CERCARE RIMEDIO SUBITO
+*/ 
+
 import React, { Component, Fragment } from 'react';
 import './registrarsi.css';
 
 
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+
 class Registrarsi extends Component {
+    // Funzioni JavaScript qui
+    
+    constructor(props, form, username, email, password, password2){
+        super(props);
+        this.form = form;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.password2 = password2;
+        console.log("SONO NEL COSTRUTTORE");
+    }
+    
+    setClick(input, form){
+        form.addEventListener("click", e => {
+            e.preventDefault();
+            console.log("HAI CLICCATO REGISTRATI");
+            regist.checkInputs();
+        });
+    }
+
+
+    setErrorFor(input, message) {
+        const formControl = input.parentElement;
+        const small = formControl.querySelector('small');
+        formControl.className = 'form-control error';
+        small.innerText = message;
+    }
+
+    setSuccessFor(input) {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control success';
+    }
+
+    isEmail(email) {
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    }
+
+    funzione(){
+        console.log("AIUTOOOOOOOOOO");
+    }
+
+    checkInputs() {
+        // trim to remove the whitespaces
+        
+        const usernameValue = username.value.trim();
+        const emailValue = email.value.trim();
+        const passwordValue = password.value.trim();
+        const password2Value = password2.value.trim();
+        
+
+        
+        
+        console.log("funzione checkInputs");
+        if(usernameValue === '') {
+            regist.setErrorFor(username, 'Inserisci username');
+            console.log("username nullo");
+        } else {
+            regist.setSuccessFor(username);
+            console.log("username ok");
+        }
+        
+        if(emailValue === '') {
+            regist.setErrorFor(email, 'Inserisci email');
+            console.log("email nulla");
+        } else if (!regist.isEmail(emailValue)) {
+            regist.setErrorFor(email, 'Email non valida');
+            console.log("email NON valida");
+        } else {
+            regist.setSuccessFor(email);
+            console.log("email ok");
+        }
+        
+        if(passwordValue === '') {
+            console.log("password nulla");
+            regist.setErrorFor(password, 'Inserisci password');
+        } else {
+            console.log("password ok");
+            regist.setSuccessFor(password);
+        }
+        
+        if(password2Value === '') {
+            console.log("password DUE nulla");
+            regist.setErrorFor(password2, 'Inserisci password');
+        } else if(passwordValue !== password2Value) {
+            console.log("Password diversa");
+            regist.setErrorFor(password2, 'Le password non corrispondono');
+        } else{
+            console.log("password ok");
+            regist.setSuccessFor(password2);
+        }
+    }
+
+    // QUESTO È IL MAIN()
+
     render() {
+
+        
         return (
 
             <Fragment>
-                
                 <div id="cont" className="container">
                     <div className="header">
                         <h2>Create Account</h2>
@@ -17,11 +127,13 @@ class Registrarsi extends Component {
                         <label htmlFor="username">Username</label>
                         <input type="text" id="username" />
                         <small>Error message</small>
+
                     </div>
                     <div className="form-control">
                         <label htmlFor="username">Email</label>
                         <input type="email" id="email" />
                         <small>Error message</small>
+
                     </div>
                     <div className="form-control">
                         <label htmlFor="username">Password</label>
@@ -33,14 +145,19 @@ class Registrarsi extends Component {
                         <input type="password" id="password2"/>
                         <small>Error message</small>
                     </div>
-                    <button>Registrati</button>
+                    <button onClick={console.log("FINO A QUI...")}>Registrati
+                        
+                    </button>
                 </form>
                 </div>
-
+                
             </Fragment> 
-            
+
         );
+
     }
 }
+
+const regist = new Registrarsi(username, email, password, password2);
 
 export default Registrarsi;
