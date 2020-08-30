75143e086b11b7f289871448cadffad854181223
Gasu16 <rosarioma.grammatico@studenti.unicam.it>
2020-08-30 23:51:05 +0200

    Query SQL DB risolte

--- server/database/databasemanage.js
+++ server/database/databasemanage.js
@@ -1,22 +1,29 @@
 var sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('../databaseManage.db', sqlite3.OPEN_READWRITE, (err) => {
-  if(err){
-    console.log("Ecco un errore in databasemanage.js");
-    console.error(err.message);
-  }
-  console.log("Database connesso correttamente");
-});
-
-//var db = new sqlite3.Database(':memory:');
-// Set database
-db.serialize(function() {
-  let sql = 'select nome from REGIONE'; 
-  db.all(sql, [], (err, rows) => {
-    if(err)throw err; // errore
-    rows.forEach((row) => {
-      console.log(row);
+class DatabaseManage{
+  DatabaseManage(){
+    console.log("provaprovaprova");
+    /*
+    let db = new sqlite3.Database('../databaseManage.db', sqlite3.OPEN_READWRITE, (err) => {
+      if(err){
+        console.log("Ecco un errore in databasemanage.js");
+        console.error(err.message);
+      }
+      console.log("Database connesso correttamente");
     });
-  });
-});
+    db.serialize(function() {
+      let sql = 'select nome from REGIONE'; 
+      db.all(sql, [], (err, rows) => {
+        if(err)throw err; // errore
+        rows.forEach((row) => {
+          console.log(row);
+        });
+      });
+    });
+    
+    db.close();
+*/  
+  } 
+}
 
-db.close();
+const dbHandle = new DatabaseManage();
+module.exports = DatabaseManage;
--- server/routes/submit.js
+++ server/routes/submit.js
@@ -1,7 +1,12 @@
 var express = require('express');
 const app = require('../app');
+//const DatabaseManage = require('../database/databasemanage');
 var router = express.Router();
 
+// Da cambiare per Stefano
+const dbPath = '/home/mob/AnAvis/server/databaseManage.db';
+
+var sqlite3 = require('sqlite3').verbose();
 /* GET about page. */
 
 router.get('/', function(req, res, next) {
@@ -10,8 +15,29 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
-    res.render('submit', {title: 'Submit'}); // mostriamo la pagina submit
     
+    res.render('submit', {title: 'Submit'});
+    next();
 });
 
+router.use(function(req, res, next){
+    let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+        if(err){
+          console.log("Ecco un errore in databasemanage.js");
+          console.error(err.message);
+        }
+        console.log("Database connesso correttamente");
+      });
+      db.serialize(function() {
+        let sql = 'select nome from REGIONE'; 
+        db.all(sql, [], (err, rows) => {
+          if(err)throw err; // errore
+          rows.forEach((row) => {
+            console.log(row);
+          });
+        });
+      });
+      
+      db.close();
+});
 module.exports = router;
